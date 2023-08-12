7f04b229cc90def83731e3d979b374d2f603f1da
PREMIUMDATA <leoedidiong@gmail.com>
2023-08-12 09:34:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - An entry point of the program
+ * Return:0 (indicate success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
